9b184aeb1224e2afb2a4a3430e17e27705743557
Martin Kröger <mk@mat.ethz.ch>
2023-09-26 17:16:59 +0200

    Update find_edges_for_p.m

--- find_edges_for_p.m
+++ find_edges_for_p.m
@@ -1,5 +1,5 @@
 function [Ps,triangle_nos] = find_edges_for_p(L,x,id,A,B,p,MaxPoreRadius,rs)
-MAYBE REPLACES BY find_triangle_for_p and find_edges_for_triangle
+% MAYBE REPLACES BY find_triangle_for_p and find_edges_for_triangle
     D   = fold(L,bsxfun(@minus,x,p)); % may be replaced by neighbor list here
     d   = sqrt(sum(D'.^2));
     is  = find(d<=2*MaxPoreRadius+rs);
